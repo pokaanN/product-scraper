89e39e449a2b9f28b81bb537245137272bedf313
pokaanN <fpxszgreqwovknb@cskindopro.com>
2026-06-10 00:33:09 +0530

    Update formatter.js

--- formatter.js
+++ formatter.js
@@ -1,1 +1,61 @@
 
+import { createMD5 } from "./md5.js";
+
+export function formatProduct(product) {
+
+const id =
+Date.now().toString();
+
+return {
+
+```
+id,
+product_id: id,
+
+name: product.name || "",
+
+color: "Default",
+
+size: "Free Size",
+
+storage: "",
+
+selling_price:
+  product.price || "",
+
+mrp:
+  product.mrp || "",
+
+fetaures:
+  product.description || "",
+
+from_csv: "1",
+
+created_at:
+  new Date()
+    .toISOString()
+    .replace("T", " ")
+    .substring(0, 19),
+
+md5_id:
+  createMD5(id),
+
+verients: [],
+
+img1:
+  product.images?.[0] || "",
+
+img2:
+  product.images?.[1] || "",
+
+img3:
+  product.images?.[2] || "",
+
+img4:
+  product.images?.[3] || "",
+
+features: ""
+```
+
+};
+}
